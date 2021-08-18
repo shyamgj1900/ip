a422969d1893a11a2f0138691b1bab29b141581d
Shyam <e0544484@u.nus.edu>
2021-08-18 10:56:54 +0800

    Greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        System.out.println("-----------------------------------");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
+        System.out.println("-----------------------------------");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("-----------------------------------");
     }
 }
