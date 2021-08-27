6de93ebf1cd9ca2cb4336fc11ec4ed9d6a33b345
Shyam <e0544484@u.nus.edu>
2021-08-27 23:28:01 +0800

    Modify code to comply with OOP

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,19 +5,58 @@ public class Duke {
 
     public static void main(String[] args) {
         Scanner myWord = new Scanner(System.in);
-
-        System.out.println("-----------------------------------");
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        System.out.println("-----------------------------------");
-
+        Task[] t = new Task[100];
+        int ctr = 0;
+        welcomeMessage();
         String word = "";
         while (!Objects.equals(word, "bye")) {
             word = myWord.nextLine();
-            if (!Objects.equals(word, "bye")) {
-                Task t = new Task();
-                Task.addList(word);
+            if (Objects.equals(word, "list")) {
+                printList(t, ctr);
+            } else if (word.contains("done")) {
+                markDone(t, word);
+            } else if (Objects.equals(word, "bye")) {
+                exitMessage();
+            } else {
+                ctr = createTask(t, ctr, word);
             }
         }
+    }
+
+    private static int createTask(Task[] t, int ctr, String word) {
+        t[ctr] = new Task(word);
+        System.out.println("-----------------------------------");
+        System.out.println("added: " + t[ctr].getTask());
+        System.out.println("-----------------------------------");
+        ctr++;
+        return ctr;
+    }
+
+    private static void markDone(Task[] t, String word) {
+        String l = word.substring(word.length() - 1);
+        int idx = Integer.parseInt(l);
+        t[idx - 1].setDone();
+        System.out.println("-----------------------------------");
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("[" + t[idx - 1].getStatusIcon() + "] " + t[idx - 1].getTask());
+        System.out.println("-----------------------------------");
+    }
+
+    private static void printList(Task[] t, int ctr) {
+        System.out.println("-----------------------------------");
+        for (int i = 0; i < ctr; i++) {
+            System.out.println(i + 1 + ". " + "[" + t[i].getStatusIcon() + "] " + t[i].getTask());
+        }
+        System.out.println("-----------------------------------");
+    }
+
+    private static void welcomeMessage() {
+        System.out.println("-----------------------------------");
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("-----------------------------------");
+    }
+
+    private static void exitMessage() {
         System.out.println("-----------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("-----------------------------------");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,36 +1,26 @@
-import java.util.Objects;
+
 
 public class Task {
     private static int listSize = 0;
-    private static String[] textList = new String[100];
-    private static boolean[] isDone = new boolean[100];
+    public static final int MAX_SIZE = 100;
+    protected String task;
 
-    public static String getStatusIcon(int i) {
-        return (isDone[i] ? "X" : " ");
+    protected boolean isDone;
+
+    public Task(String task) {
+        this.task = task;
+        this.isDone = false;
     }
 
-    public static void addList(String word) {
-        if (Objects.equals(word, "list")) {
-            System.out.println("-----------------------------------");
-            for (int i = 0; i < listSize; i++) {
-                System.out.println(i + 1 + ". " + "[" + getStatusIcon(i) + "] " + textList[i]);
-            }
-            System.out.println("-----------------------------------");
-        } else if (word.contains("done")) {
-            String l = word.substring(word.length() - 1);
-            int idx = Integer.parseInt(l);
-            isDone[idx - 1] = true;
-            System.out.println("-----------------------------------");
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println("[" + getStatusIcon(idx - 1) + "] " + textList[idx - 1]);
-            System.out.println("-----------------------------------");
-        } else {
-            textList[listSize] = word;
-            isDone[listSize] = false;
-            System.out.println("-----------------------------------");
-            System.out.println("added: " + textList[listSize]);
-            System.out.println("-----------------------------------");
-            listSize++;
-        }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setDone() {
+        isDone = true;
     }
 }
